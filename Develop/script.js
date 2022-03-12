eaed782a9f775a387a3efd587cf94c8c951fdde4
Obie <obiemunozjr@gmail.com>
2022-03-11 21:49:19 -0500

    Added functionality for user to select password characters and generate password

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,13 @@
-var passwordInfo = {
+let passwordInfo = {
   value: "",
   length: 8
 }
 
-var passwordLengthCheck = function() {
-  var passwordLength = window.prompt("Please specify number of characters from 8 to 128.");
+let passwordLengthCheck = function () {
+  let passwordLength = window.prompt("Please specify number of characters from 8 to 128.");
   passwordLength = parseInt(passwordLength);
   console.log(passwordLength);
 
-  // Check to make sure the entry is between 8 and 128 characters
   if (passwordLength >= 8 && passwordLength <= 128) {
     passwordInfo.length = passwordLength;
   } else {
@@ -20,21 +19,55 @@ var passwordLengthCheck = function() {
 
 function generatePassword() {
   passwordLengthCheck();
+  let passwordCharacters = selectCharacters();
+  let password = "";
+  for (let i = 0; i < passwordInfo.length; i++) {
+    let random = Math.floor(Math.random() * passwordCharacters.length);
+    password += passwordCharacters[random];
+  }
+
+  passwordInfo.value = password;
+  return passwordInfo.value;
 };
 
-// Calculates the number of characters.
-var passwordLength = function(min, max) {
+function selectCharacters() {
+  let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let numbers = "0123456789";
+  let specialCharacters = "!@#$%^&*()_+";
+  let lowerCaseCheck = window.confirm("Would you like to include lowercase letters?");
+  let upperCaseCheck = window.confirm("Would you like to include uppercase letters?");
+  let numbersCheck = window.confirm("Would you like to include numbers?");
+  let specialCharactersCheck = window.confirm("Would you like to include special characters?");
+  let passwordCharacters = "";
+
+  if (lowerCaseCheck) {
+    passwordCharacters += lowerCase;
+  }
+  if (upperCaseCheck) {
+    passwordCharacters += upperCase;
+  }
+  if (numbersCheck) {
+    passwordCharacters += numbers;
+  }
+  if (specialCharactersCheck) {
+    passwordCharacters += specialCharacters;
+  }
+  return passwordCharacters;
+}
+
+let passwordLength = function (min, max) {
 
 
 };
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
